8a3a323ee4926aaa79f4f20f020b038121a2b1a4
harish-ramar <personal.harishramar@outlook.com>
2025-08-22 23:59:24 +0530

    fix vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/trust-website",
+  base: "/trust-website/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
